215ee627bdbe70d69f7e7406408fffba78a094d6
yxu <penguinxx2@gmail.com>
2024-01-27 01:47:12 +0900

    rebuilded

--- client_srcs/client.c
+++ client_srcs/client.c
@@ -6,48 +6,140 @@
 /*   By: yxu <yxu@student.42tokyo.jp>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/20 18:02:32 by yxu               #+#    #+#             */
-/*   Updated: 2024/01/21 23:56:34 by yxu              ###   ########.fr       */
+/*   Updated: 2024/01/27 01:45:34 by yxu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minitalk.h"
 
+/*                    32byte-package                         */
+/*  [ PID bytes(4B) | message bytes(24B) | check bytes(4B)]  */
+/*     client PID    6 utf8 char 1char/4B     sum of 28B     */
+
+static void	exit_with_msg(int exitcode, char *msg)
+{
+	ft_printf("%s\n", msg);
+	exit(exitcode);
+}
+
 static void	send_byte(pid_t pid, char byte)
 {
 	int	bit_i;
 	int	bit;
+	int	kill_flag;
 
 	bit_i = 0;
 	while (bit_i < BIT_PER_BYTE)
 	{
 		bit = byte >> bit_i++ & 0b00000001;
 		if (bit == 0)
-			kill(pid, SIGUSR1);
+			kill_flag = kill(pid, SIGUSR1);
 		if (bit == 1)
-			kill(pid, SIGUSR2);
-		usleep(1000);
+			kill_flag = kill(pid, SIGUSR2);
+		if (kill_flag != 0)
+			exit_with_msg(0, "Message sending failed. \
+Please check the status of server and pid\n");
+		usleep(200);
 	}
 }
 
-static void	send_end_of_msg(pid_t pid)
+static void	send_package(pid_t clientpid, pid_t serverpid, char msg[24])
 {
-	send_byte(pid, 0xFF);
-	send_byte(pid, 0xFF);
-	send_byte(pid, 0xFF);
-	send_byte(pid, 0xFF);
+	static int	count = 0;
+	char		package[32];
+	int			checkbyte;
+	int			i;
+
+	((int *)package)[0] = clientpid;
+	i = 0;
+	while (i < 24)
+	{
+		package[4 + i] = msg[i];
+		i++;
+	}
+	i = 0;
+	checkbyte = 0;
+	while (i < 28)
+		checkbyte += package[i++];
+	((int *)package)[7] = checkbyte;
+	i = 0;
+	while (i < 32)
+		send_byte(serverpid, package[i++]);
+	ft_printf("package%d sent\n", count++);
+}
+
+static int	byte_of_utf8(unsigned char c)
+{
+	unsigned char	first4bit;
+
+	first4bit = (c >> 4) & 0b00001111;
+	if (first4bit <= 0b0111)
+		return (1);
+	else if (first4bit >= 0b1100 && first4bit <= 0b1101)
+		return (2);
+	else if (first4bit == 0b1110)
+		return (3);
+	else if (first4bit == 0b1111)
+		return (4);
+	else
+	{
+		ft_printf("Error while decoding.\n");
+		exit(0);
+	}
+}
+
+static char	*get_next_msg(char msg[24], char *input)
+{
+	int		byte_of_char;
+	int		utf8char_i;
+	int		byte_i;
+
+	utf8char_i = 0;
+	if (input == NULL)
+		return (NULL);
+	while (*input && utf8char_i < 6)
+	{
+		byte_of_char = byte_of_utf8(*input);
+		byte_i = 0;
+		while (byte_i < byte_of_char)
+			msg[4 * utf8char_i + byte_i++] = *input++;
+		utf8char_i++;
+	}
+	return (input);
+}
+
+static void	deal_signal(int signum)
+{
+	static int	count = 0;
+
+	if (signum == SIGUSR2)
+		ft_printf("package%d received correctly\n", count++);
 }
 
 int	main(int argc, char **argv)
 {
-	pid_t	pid;
-	int		char_i;
+	pid_t	serverpid;
+	pid_t	clientpid;
+	char	*input;
+	char	msg[24];
 
 	if (argc != 3)
-		exit(0);
-	pid = ft_atoi(argv[1]);
-	char_i = 0;
-	while (argv[2][char_i])
-		send_byte(pid, argv[2][char_i++]);
-	send_end_of_msg(pid);
+		exit_with_msg(0, "There must be 2 arguments: PID and message");
+	serverpid = ft_atoi(argv[1]);
+	clientpid = getpid();
+	signal(SIGUSR1, deal_signal);
+	signal(SIGUSR2, deal_signal);
+	if (serverpid == 0)
+		exit_with_msg(0, "Wrong PID\n");
+	input = argv[2];
+	while (*input)
+	{
+		ft_memset(msg, 0, 24);
+		input = get_next_msg(msg, input);
+		send_package(clientpid, serverpid, msg);
+	}
+	ft_memset(msg, 0, 24);
+	msg[0] = '\n';
+	send_package(clientpid, serverpid, msg);
 	return (0);
 }
--- server_srcs/server.c
+++ server_srcs/server.c
@@ -6,15 +6,21 @@
 /*   By: yxu <yxu@student.42tokyo.jp>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/20 17:16:21 by yxu               #+#    #+#             */
-/*   Updated: 2024/01/22 00:59:43 by yxu              ###   ########.fr       */
+/*   Updated: 2024/01/27 01:46:41 by yxu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minitalk.h"
 
+static void	exit_with_msg(int exitcode, char *msg)
+{
+	ft_printf("%s\n", msg);
+	exit(exitcode);
+}
+
 static int	byte_of_utf8(unsigned char c)
 {
-	unsigned char first4bit;
+	unsigned char	first4bit;
 
 	first4bit = (c >> 4) & 0b00001111;
 	if (first4bit <= 0b0111)
@@ -32,19 +38,39 @@ static int	byte_of_utf8(unsigned char c)
 	}
 }
 
-static int	is_end_of_msg(int byte_of_c, unsigned char *c)
+static void	parse_package(char package[32])
 {
-	if (byte_of_c == 4)
-		if (c[0] == 0xFF || c[1] == 0xFF || c[2] == 0xFF || c[3] == 0xFF)
-			return (1);
-	return (0);
+	int		checkbyte;
+	int		i;
+	int		kill_flag;
+
+	i = 0;
+	checkbyte = 0;
+	while (i < 28)
+		checkbyte += package[i++];
+	if (((int *)package)[7] == checkbyte)
+	{
+		i = 0;
+		while (i < 6)
+		{
+			write(1, &package[4 + 4 * i], byte_of_utf8(package[4 + 4 * i]));
+			i++;
+		}
+		kill_flag = kill(((int *)package)[0], SIGUSR2);
+	}
+	else
+		ft_printf("An error occured when receiving message. \
+Here are some possible reasons:\n\
+1: More than one clients were sending message at the same time.\n\
+2: Bad status...\n");
+	if (kill_flag != 0)
+		exit_with_msg(0, "something wrong with sending success signal\n");
 }
 
-static void	parse_byte(int signum)
+static void	deal_signal(int signum)
 {
-	static unsigned char	c[4] = {0};
+	static char	package[32] = {0};
 	static int	count = 0;
-	static int	byte_of_c = 1;
 	int			bit;
 
 	if (signum == SIGUSR1)
@@ -52,27 +78,22 @@ static void	parse_byte(int signum)
 	else if (signum == SIGUSR2)
 		bit = 1;
 	if (bit == 0)
-		c[count / 8] = (c[count / 8] >> 1) & 0b01111111;
+		package[count / 8] = (package[count / 8] >> 1) & 0b01111111;
 	else if (bit == 1)
-		c[count / 8] = (c[count / 8] >> 1) | 0b10000000;
+		package[count / 8] = (package[count / 8] >> 1) | 0b10000000;
 	count++;
-	if (count == 8)
-		byte_of_c = byte_of_utf8(c[0]);
-	if (count == byte_of_c * 8)
+	if (count == 256)
 	{
-		if (is_end_of_msg(byte_of_c, c))
-			ft_printf("\n");
-		else
-			write(1, c, byte_of_c);
 		count = 0;
+		parse_package(package);
 	}
 }
 
 int	main(void)
 {
 	ft_printf("Server start\nPID: %d\n", getpid());
-	signal(SIGUSR1, parse_byte);
-	signal(SIGUSR2, parse_byte);
+	signal(SIGUSR1, deal_signal);
+	signal(SIGUSR2, deal_signal);
 	while (1)
 		;
 	return (0);
